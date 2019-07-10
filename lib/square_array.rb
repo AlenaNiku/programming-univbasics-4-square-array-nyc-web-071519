ddca6e8a0a53ed0ef516d28a6c843deecc34030d
Alena Nikulina <alenanikulina0@gmail.com>
2019-07-10 15:39:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
  num = 0
  new_array = []
  while num < array.length do
-   new_array.push(array[index]**2)
+   new_array.push(array[num]**2)
    num += 1
  end
  return new_array
